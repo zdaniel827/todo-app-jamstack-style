5178921f4e8a12a31eb9466ab920331a45481080
Zachary Daniel <zdaniel827@gmail.com>
2020-04-10 21:51:25 -0700

    deletion of todos

--- src/component/todo.js
+++ src/component/todo.js
@@ -12,6 +12,15 @@ const Todo = ({ todo, reloadTodos }) => {
       })
       .then(reloadTodos);
   };
+
+  const handleDelete = () => {
+    axios
+      .post("/api/delete-todo", {
+        id: todo._id,
+      })
+      .then(reloadTodos);
+  };
+
   return (
     <>
       <label htmlFor={`todo-toggle-${todo._id}`} className={styles.label}>
@@ -27,6 +36,14 @@ const Todo = ({ todo, reloadTodos }) => {
       <p className={`${styles.text} ${todo.completed && styles.completed}`}>
         {todo.text}
       </p>
+      <label htmlFor={`todo-delete-${todo._id}`} className={styles.label}>
+        delete
+      </label>
+      <button onClick={handleDelete} className={styles.delete}>
+        <span role='img' aria-label='delete' title='delete this todo'>
+          ❌
+        </span>
+      </button>
     </>
   );
 };
